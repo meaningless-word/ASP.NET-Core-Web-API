34ff17eeded1e2d4555d177737ed8655d5054fdc
Роман Толмачёв <unifiq@gmail.com>
2021-08-18 14:24:32 +0700

    2

---
+++ HomeApi/HomeApi/Controllers/DevicesController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using HomeApi.Configuration;
+using HomeApi.Contracts.Models.Devices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace HomeApi.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class DevicesController : ControllerBase
+	{
+		private IOptions<HomeOptions> _options;
+		private IMapper _mapper;
+
+		public DevicesController(IOptions<HomeOptions> options, IMapper mapper)
+		{
+			_options = options;
+			_mapper = mapper;
+		}
+
+		/// <summary>
+		/// Просмотр списка подключенных устройств
+		/// </summary>
+		[HttpGet]
+		[Route("")]
+		public IActionResult Get()
+		{
+			return StatusCode(200, "Устройства отсутствуют");
+		}
+
+		/// <summary>
+		/// Добавление нового устройства
+		/// </summary>
+		[HttpPost]
+		[Route("Add")]
+		public IActionResult Add(
+			[FromBody] // атрибут, указывающий откуда брать значение объекта
+			AddDeviceRequest request // объект запроса
+		)
+		{
+			if (request.CurrentVolts < 120)
+			{
+				// Добавляем для клиента информативную ошибку
+				ModelState.AddModelError("currentVolts", "Устройства с напряжением меньше 120 вольт не поддерживаются!");
+				return BadRequest(ModelState);
+			}
+
+			return StatusCode(200, $"Устройство {request.Name} добавлено!");
+		}
+	}
+}
--- HomeApi/HomeApi/Controllers/HomeController.cs
+++ HomeApi/HomeApi/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using HomeApi.Configuration;
-using HomeApi.Contracts;
+using HomeApi.Contracts.Models.Home;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System;
