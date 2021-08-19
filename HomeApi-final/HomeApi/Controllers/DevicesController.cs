b365e5aba6f34a1fcff43dcde0f98c510f297f65
Роман Толмачёв <unifiq@gmail.com>
2021-08-20 00:58:24 +0700

    delete method

--- HomeApi-final/HomeApi/Controllers/DevicesController.cs
+++ HomeApi-final/HomeApi/Controllers/DevicesController.cs
@@ -97,5 +97,17 @@ namespace HomeApi.Controllers
 
 			return StatusCode(200, $"Устройство обновлено! Имя - {device.Name}, Серийный номер - {device.SerialNumber},  Комната подключения - {device.Room.Name}");
 		}
+
+		[HttpDelete]
+		[Route("{id}")]
+		public async Task<IActionResult> Delete([FromRoute] Guid id)
+		{
+			var device = await _devices.GetDeviceById(id);
+			if (device == null)
+				return StatusCode(400, $"Ошибка: Устройство с идентификатором {id} не существует.");
+
+			await _devices.DeleteDevice(device);
+			return StatusCode(201, $"Устройства с идентификатором {id} больше нет...");
+		}
 	}
 }
